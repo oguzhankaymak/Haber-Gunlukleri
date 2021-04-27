87f617265f1572951acd131123120442eb87b25f
Oğuzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-04-27 23:18:32 +0300

    get news action create. This action is get news with api

--- app/redux/news/Actions.js
+++ app/redux/news/Actions.js
@@ -1,3 +1,14 @@
-export const getNews = () => dispatch => {
-  // GET NEWS
+import {request} from '../../utils/Request';
+import {START, SUCCESS, ERROR} from './Types';
+
+export const getNews = (country, category) => {
+  return async (dispatch, getState) => {
+    try {
+      dispatch({type: START});
+      let response = await request.get('', {params: {country, category}});
+      dispatch({type: SUCCESS, payload: response.data.articles});
+    } catch (err) {
+      dispatch({type: ERROR, payload: err});
+    }
+  };
 };
