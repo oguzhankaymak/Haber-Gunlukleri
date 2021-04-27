d7fa98c36c718744ef95e26b1324da1a650216c1
Oğuzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-04-27 23:20:19 +0300

    news get with selector from redux and listed in home

--- app/screens/home/HomeScreen.js
+++ app/screens/home/HomeScreen.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, FlatList} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
 
 import styles from './styles/HomeScreenStyles';
 import NewsCard from '../../components/home/NewsCard';
+import {getNews} from '../../redux/news/Actions';
 
 const HomeScreen = () => {
-  const data = [];
+  const dispatch = useDispatch();
+  const data = useSelector(state => state.news.news);
+
+  useEffect(() => {
+    if (!data || data.length === 0) {
+      dispatch(getNews('tr', 'sports'));
+    }
+  }, []);
 
   const renderItem = ({item}) => (
     <View style={styles.itemStyle}>
--- app/screens/home/styles/HomeScreenStyles.js
+++ app/screens/home/styles/HomeScreenStyles.js
@@ -5,7 +5,6 @@ export default StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.white,
-    paddingTop: 50,
     alignItems: 'center',
   },
 
