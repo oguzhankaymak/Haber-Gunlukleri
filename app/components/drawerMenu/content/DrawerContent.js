ade5d6ae344b7db784ccd350d09881c5321c6908
Oğuzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-05-05 00:11:04 +0300

    drawer content create

--- app/components/drawerMenu/content/DrawerContent.js
+++ app/components/drawerMenu/content/DrawerContent.js
@@ -1,42 +1,78 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
 import Colors from '../../../theme/Colors';
 
 import {NewspaperIcon} from '../../icons';
 import DrawerCard from '../card/DrawerCard';
+import Constants from './constants/Constants';
 import styles from './styles/DrawerContentStyle';
 
 const DrawerContent = props => {
+  const [contentType, setContentType] = useState(Constants.LIST);
+
+  const _renderHeader = () => (
+    <View style={styles.header}>
+      <NewspaperIcon
+        stroke={styles.icon.color}
+        width={styles.icon.width}
+        height={styles.icon.height}
+      />
+      <Text style={styles.titleText}>Son Haber</Text>
+      <Text style={styles.descriptionText}>Son Haberler Artık Cebinizde!</Text>
+    </View>
+  );
+
+  const _renderContent = () => {
+    if (contentType === Constants.LIST) {
+      return (
+        <>
+          <DrawerCard
+            title={'Ülke'}
+            value={'Türkiye'}
+            backgroundColor={Colors.purplishPink}
+            color={Colors.white}
+            onPress={() => setContentType(Constants.COUNTRY_LIST)}
+          />
+          <View style={styles.secondCard}>
+            <DrawerCard
+              title={'Kategori'}
+              value={'Teknoloji'}
+              backgroundColor={Colors.hotPink}
+              color={Colors.white}
+              onPress={() => setContentType(Constants.CATEGORY_LIST)}
+            />
+          </View>
+        </>
+      );
+    } else if (contentType === Constants.COUNTRY_LIST) {
+      return <Text>Country</Text>;
+    } else if (contentType === Constants.CATEGORY_LIST) {
+      return <Text>CATEGORY_LIST</Text>;
+    }
+  };
+
+  const _renderFooter = () => {
+    if (contentType !== Constants.LIST) {
+      return (
+        <>
+          <View style={styles.line} />
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.footer}
+            onPress={() => setContentType(Constants.LIST)}>
+            <Text style={styles.cancelText}>Vazgeç</Text>
+          </TouchableOpacity>
+        </>
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <NewspaperIcon
-          stroke={styles.icon.color}
-          width={styles.icon.width}
-          height={styles.icon.height}
-        />
-        <Text style={styles.titleText}>Son Haber</Text>
-        <Text style={styles.descriptionText}>
-          Son Haberler Artık Cebinizde!
-        </Text>
-      </View>
+      {_renderHeader()}
       <View style={styles.line} />
-      <View style={styles.cards}>
-        <DrawerCard
-          title={'Ülke'}
-          value={'Türkiye'}
-          backgroundColor={Colors.purplishPink}
-          color={Colors.white}
-        />
-        <View style={styles.secondCard}>
-          <DrawerCard
-            title={'Kategori'}
-            value={'Teknoloji'}
-            backgroundColor={Colors.hotPink}
-            color={Colors.white}
-          />
-        </View>
-      </View>
+      <View style={styles.content}>{_renderContent()}</View>
+      {_renderFooter()}
     </View>
   );
 };
