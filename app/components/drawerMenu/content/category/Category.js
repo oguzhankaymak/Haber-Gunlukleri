671d4f1d139ab294d75fc77d6866cdbfe3d1101a
Oğuzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-05-05 22:59:10 +0300

    category mock data change and checkbox type prop add

--- app/components/drawerMenu/content/category/Category.js
+++ app/components/drawerMenu/content/category/Category.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import {View, FlatList} from 'react-native';
 import Checkbox from '../../../checkbox/Checkbox';
-import {COUNTRY} from '../../../../mocks/Data';
+import {CATEGORY} from '../../../../mocks/Data';
 
 import styles from './styles/CategoryStyle';
 
 const Category = () => {
-  const key = 'tr';
+  const key = 'science';
   const renderCategoryItem = ({item}) => (
     <View style={styles.item}>
-      <Checkbox active={key === item.key} name={item.name} />
+      <Checkbox active={key === item.key} name={item.name} type={'category'} />
     </View>
   );
 
   return (
     <FlatList
       keyExtractor={(item, index) => index}
-      data={COUNTRY}
+      data={CATEGORY}
       renderItem={renderCategoryItem}
     />
   );
